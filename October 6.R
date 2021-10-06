9935c92f49712e4b7853544d53fb4dd7ed9ff1c1
Alani Williams <89275111+ALANIWilliams@users.noreply.github.com>
2021-10-06 15:47:00 -0400

    Add files via upload

---
+++ October 6.R
@@ -0,0 +1,9 @@
+# Author: Alani Williams 
+#Date: Oct 6, 2021 
+#Purpose: Calculate discrete probability distribution values
+
+#Calculate Continuous Uniform Distribution Probability 
+
+#Generate 10 random numbers between 1 and 3
+runif(10, min=1, max=3)
+
